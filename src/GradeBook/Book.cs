6c76c9f548e758b877716f781d70c07d72dc6f3b
Maximilian Lærum <max.larum@km.kongsberg.com>
2019-08-26 09:16:40 +0200

    removed a comment

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -17,7 +17,7 @@ namespace GradeBook
             grades.Add(grade);
         }
 
-        // Compute!
+        
         public Statistics GetStatistics()
         {
             var result = new Statistics();
